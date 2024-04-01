4b1f08e910543ece6a93744ecab29db1a33f83e6
zeenat <zeenu.23@gmail.com>
2024-04-01 23:01:41 +0530

    Add axios for API calls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage, setPostsPerPage] = useState(10)
+
+  useEffect(() => {
+    
+  }, [])
+  
+
   return (
-    <div>
+    <div className="container">
       
     </div>
   );
